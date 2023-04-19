c6a1082baa49cbbb27b4fa59ce3147bc8d852656
hristo.iliev <hristo.iliev@info.nl>
2023-04-19 19:40:15 +0300

    feat: replace lens with multiverse

--- images/generate-gallery-snippet.sh
+++ images/generate-gallery-snippet.sh
@@ -5,16 +5,16 @@ set -e
 gallery_dir=$1
 number_of_images=$2
 
-echo "				<!-- Thumbnail -->
-				<section id=\"thumbnails\">"
+echo "				<!-- Main -->
+				<div id=\"main\">"
 
 for i in $(seq 1 "$number_of_images"); do
   formatted_num=$(printf "%03d" "$i")
-  echo "				<article>
-            <a class=\"thumbnail\" href=\"images/gallery/$gallery_dir/fulls/$formatted_num.jpg\" data-position=\"left center\"><img src=\"images/gallery/$gallery_dir/thumbs/$formatted_num.jpg\" alt=\"\" /></a>
-            <h2></h2>
-            <p></p>
-				</article>"
+  echo "					<article class=\"thumb\">
+        						<a href=\"images/gallery/$gallery_dir/fulls/$formatted_num.jpg\" class=\"image\"><img src=\"images/gallery/$gallery_dir/thumbs/$formatted_num.jpg\" alt=\"\" /></a>
+        						<h2></h2>
+        						<p></p>
+        					</article>"
 done
 
-echo "				</section>"
+echo "				</div>"

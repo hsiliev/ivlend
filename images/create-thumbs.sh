42987185cd3fe5e626df55ea5c1283038d109aa1
hristo.iliev <hristo.iliev@info.nl>
2023-04-19 20:09:41 +0300

    feat: resize thumbnails to x400

--- images/create-thumbs.sh
+++ images/create-thumbs.sh
@@ -7,5 +7,5 @@ mkdir -p gallery/"$1"/thumbs
 for path in gallery/"$1"/fulls/*; do
   file=$(basename "${path}")
   echo "Processing image $file ..."
-  convert -thumbnail x250 "$path" gallery/"$1/thumbs/$file"
+  convert -thumbnail x400 "$path" gallery/"$1/thumbs/$file"
 done

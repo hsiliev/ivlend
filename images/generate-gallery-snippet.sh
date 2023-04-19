b827fc366a729c7cd74afbaf801a2ae0f61f612d
hristo.iliev <hristo.iliev@info.nl>
2023-04-19 12:34:49 +0300

    feat: use lens theme as gallery

--- images/generate-gallery-snippet.sh
+++ images/generate-gallery-snippet.sh
@@ -2,21 +2,19 @@
 
 set -e
 
-number_of_images=$1
-gallery_dir=$2
+gallery_dir=$1
+number_of_images=$2
+
+echo "				<!-- Thumbnail -->
+				<section id=\"thumbnails\">"
 
 for i in $(seq 1 "$number_of_images"); do
   formatted_num=$(printf "%03d" "$i")
-  echo "            <article>
-                <a href=\"images/gallery/$gallery_dir/fulls/$formatted_num.jpg\" class=\"image\">
-                    <img src=\"images/gallery/$gallery_dir/thumbs/$formatted_num.jpg\" alt=\"\"/>
-                </a>
-                <div class=\"caption\">
-                    <h3></h3>
-                    <p></p>
-                    <ul class=\"actions fixed\">
-                        <li><span class=\"button small\">Увеличи</span></li>
-                    </ul>
-                </div>
-            </article>"
+  echo "				<article>
+            <a class=\"thumbnail\" href=\"images/gallery/$gallery_dir/fulls/$formatted_num.jpg\" data-position=\"left center\"><img src=\"images/gallery/$gallery_dir/thumbs/$formatted_num.jpg\" alt=\"\" /></a>
+            <h2></h2>
+            <p></p>
+				</article>"
 done
+
+echo "				</section>"
